555413ace9b3cf2126b1919d4b6a443ae32709c2
CJ Meeks <cjmeeks@ksu.edu>
2016-02-08 20:31:12 -0500

    Updated applicant model

--- application/models/ApplicantModel.php
+++ application/models/ApplicantModel.php
@@ -9,4 +9,51 @@ class ApplicantModel extends MY_Model{
     $this->id = 'applicantId';
   }
 
+  function getByApplicantId($applicantId){
+    if (!is_numeric($applicantId) || $applicantId < 1) {
+        return false;
+    }
+
+    // select * from `applicant` where `applicantId` = '$applicantId'
+    $this->db->where('applicantId', $applicantId);
+
+    // Run the query on 'applicant' and get the result
+    $result = $this->db->get($this->table);
+
+    if ($result->num_rows() === 1) {
+        return $result->row_array();
+    }
+    return false;
+  }
+
+  function getByHousholdId($householdId){
+    if (!is_numeric($householdId) || $householdId < 1) {
+        return false;
+    }
+
+    // select * from `applicant` where `applicantId` = '$applicantId'
+    $this->db->where('householdId', $householdId);
+
+    // Run the query on 'applicant' and get the result
+    $result = $this->db->get($this->table);
+
+    if ($result->num_rows() > 0) {
+        return $result->result_array();
+    }
+    return false;
+  }
+
+  function getByPhone($phone){
+    if (!is_numeric($phone) || $phone < 1) {
+        return false;
+    }
+    $this->db->where('phone', $phone);
+    $result = $this->db->get($this->table);
+
+    if ($result->num_rows() === 1) {
+        return $result->row_array();
+    }
+    return false;
+  }
+
 }
