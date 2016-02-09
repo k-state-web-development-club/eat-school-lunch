a10e757d8e005c464e905853881cefda5134bbf5
DJ Bamba <djbamba@live.com>
2016-02-08 19:55:16 -0500

    add ApplicationModel.php

---
+++ application/models/ApplicationModel.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ApplicationModel extends CI_Controller {
+    var $applicationId ='';
+    function __construct(){
+        parent::construct();
+    }
+}
