4fb5af3afcdc97d6231487e0c38f7262b7ff8a1e
Joe Bennett <jebennett099@gmail.com>
2016-02-11 15:06:13 -0500

    Fix adult income model

--- application/models/AdultIncomeModel.php
+++ application/models/AdultIncomeModel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class AduleIncomeModel extends MY_Model {
+class AdultIncomeModel extends MY_Model {
 
   function __construct() {
     parent:: __construct();
