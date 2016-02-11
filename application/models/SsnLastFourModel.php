a214c2ff14c9c5560f5c1168c757202c9a3bb4a2
marygrace <mblair021@gmail.com>
2016-02-11 14:47:29 -0500

    Ssn Last Four

--- application/models/SsnLastFourModel.php
+++ application/models/SsnLastFourModel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class AdultIncomeTypeModel extends My_Model {
+class SsnLastFourModel extends My_Model {
 
     function __construct() {
       parent::_construct();
