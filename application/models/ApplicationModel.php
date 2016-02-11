aeff0465e1da3794c80c8124d4c1bcb69d6cfc3b
Joe Bennett <jebennett099@gmail.com>
2016-02-11 15:32:27 -0500

    Fix application model

--- application/models/ApplicationModel.php
+++ application/models/ApplicationModel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ApplicationModel extends CI_Controller {
+class ApplicationModel extends MY_Model {
     function __construct(){
         parent::__construct();
         $this->table = 'application';
