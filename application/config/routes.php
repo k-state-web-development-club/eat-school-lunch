d4acd80b00a18ca732b379dd4bb2a95916d0617d
Joe Bennett <jebennett099@gmail.com>
2016-02-11 20:08:45 -0500

    Configure routes and handlers for about view and steps views

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Application';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['about'] = 'Application/about';
+$route['step/(:num)'] = 'Application/step/$1';
--- application/controllers/Application.php
+++ application/controllers/Application.php
@@ -3,18 +3,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Application extends CI_Controller {
 
-    public function __construct() {
+    function __construct() {
         parent::__construct();
     }
 
     // URL: http://<username>.atodd.me
-    public function index() {
-        // This is the home page
-        $this->twig->display('welcome_message', array('testVariable' => "Hello, World!"));
+    function index() {
+        // The $data array will be passed to the view
+        // and it's values can be accessed through their
+        // corresponding key
+        $data = array(
+            'testVariable' => 'Hello, World!'
+        );
+        $this->twig->display('welcome_message', $data);
     }
 
     // URL: http://<username>.atodd.me/about
-    public function about() {
-        $this->twig->display('about', array('testVariable' => "Hello, World!"));
+    function about() {
+        $data = array(
+            'testVariable' => 'Hello, World!'
+        );
+        $this->twig->display('about', $data);
     }
+
+    // URL: http://<username>.atodd.me/step/$1
+    function step($stepNum = false) {
+        $data = array(
+            'testVariable' => 'Hello, World!'
+        );
+
+        // If no step number was provided, 404
+        if (!$stepNum)
+            show_404();
+
+        // Similarly, 404 if there was one but
+        // we do not have a view for that step
+        if (!file_exists(APPPATH.'views/steps/'.$stepNum.'.twig'))
+            show_404();
+
+        $this->twig->display('steps/'.$stepNum, $data);
+    }
+
 }
